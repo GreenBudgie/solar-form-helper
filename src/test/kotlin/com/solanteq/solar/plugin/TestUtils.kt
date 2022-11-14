55d56d4588479842e012015882a185c82060034d
Nikita Bundin <nbundin@solanteq.com>
2022-11-15 01:52:58 +0300

    Add form reference test

--- src/test/kotlin/com/solanteq/solar/plugin/TestUtils.kt
+++ src/test/kotlin/com/solanteq/solar/plugin/TestUtils.kt
@@ -5,15 +5,64 @@ import com.intellij.testFramework.fixtures.JavaCodeInsightTestFixture
 import com.solanteq.solar.plugin.file.FormFileType
 
 /**
- * Copies forms form testData directory to the proper location and opens the first form in editor
+ * Creates new form in the correct directory with specified text
+ *
+ * @param formName Form file name (without .json extension)
+ * @param text Text to be placed in a form
+ * @param group Form group. The file will be placed inside this directory
+ *
+ * @return Created form virtual file
  */
-fun JavaCodeInsightTestFixture.configureByForms(vararg formPaths: String): PsiFile? {
+fun JavaCodeInsightTestFixture.createForm(
+    formName: String,
+    text: String,
+    group: String? = null
+): PsiFile {
+    val realFileName = "$formName.json"
+    val groupPath = if (group == null) "" else "$group/"
+
+    return addFileToProject(
+        "main/resources/config/forms/$groupPath$realFileName",
+        text
+    )
+}
+
+/**
+ * Creates new form in the correct directory with specified text and opens it in the editor
+ *
+ * @see createForm
+ * @return Opened form psi file
+ */
+fun JavaCodeInsightTestFixture.createFormAndConfigure(
+    formName: String,
+    text: String,
+    group: String? = null
+): PsiFile {
+    val virtualFormFile = createForm(formName, text, group)
+    configureFromExistingVirtualFile(virtualFormFile.virtualFile)
+    return file
+}
+
+/**
+ * Copies forms from testData directory to the correct directory and opens the first form in editor
+ */
+fun JavaCodeInsightTestFixture.configureByForms(vararg formPaths: String, group: String? = null): PsiFile? {
+    val groupPath = if(group == null) "" else "$group/"
     val virtualFiles = formPaths.map {
-        copyFileToProject(it, "main/resources/config/forms/$it")
+        copyFileToProject(it, "main/resources/config/forms/$groupPath$it")
     }
     virtualFiles.firstOrNull()?.let { configureFromExistingVirtualFile(it) }
     return file
 }
 
-fun JavaCodeInsightTestFixture.configureByFormText(formText: String): PsiFile =
-    this.configureByText(FormFileType, formText)
+/**
+ * Creates a new form file and opens it in the editor with the specified text.
+ *
+ * Caution: this method only imitates form file behavior,
+ * but actually it is stored in aaa.json file in root directory.
+ * For example, references to this form will not work.
+ * For many purposes it is better to use [createFormAndConfigure] because it
+ * will be placed into correct directory.
+ */
+fun JavaCodeInsightTestFixture.configureByFormText(text: String): PsiFile =
+    this.configureByText(FormFileType, text)
