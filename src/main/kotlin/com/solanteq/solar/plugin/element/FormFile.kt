0cf235fabd72eeaec1bd33579343eaa434b47a27
Nikita Bundin <nbundin@solanteq.com>
2022-11-19 17:15:57 +0300

    FormFile now has lazy properties

--- src/main/kotlin/com/solanteq/solar/plugin/element/FormFile.kt
+++ src/main/kotlin/com/solanteq/solar/plugin/element/FormFile.kt
@@ -8,36 +8,38 @@ import com.solanteq.solar.plugin.util.valueAsString
  * Represents a form file (not included)
  */
 class FormFile(
-    sourceElement: JsonFile,
-    private val topLevelObject: JsonObject
+    sourceElement: JsonFile
 ) : FormElement<JsonFile>(sourceElement) {
 
-    fun getFullName(): String? {
-        val name = getName() ?: return null
-        val module = getModule() ?: return name
-        return "$module.$name"
+    val topLevelObject by lazy { sourceElement.topLevelValue as? JsonObject }
+
+    val fullName by lazy {
+        val name = name ?: return@lazy null
+        val module = module ?: return@lazy name
+        return@lazy "$module.$name"
     }
 
-    fun getModule() = topLevelObject.findProperty("module").valueAsString()
+    val module by lazy { topLevelObject?.findProperty("module").valueAsString() }
 
-    fun getName() = topLevelObject.findProperty("name").valueAsString()
+    val name by lazy { topLevelObject?.findProperty("name").valueAsString() }
 
     /**
-     * Gets list of all requests in this form. Possible requests are:
+     * List of all requests in this form. Possible requests are:
      * - source
      * - remove
      * - save
      * - createSource
      */
-    fun getRequests(): List<FormRequest> =
+    val requests by lazy {
         FormRequest.RequestType.formRequests.mapNotNull {
-            topLevelObject.findProperty(it.requestLiteral).toFormElement()
+            topLevelObject?.findProperty(it.requestLiteral).toFormElement()
         }
+    }
 
     /**
      * Gets the request by its type, or null if such request isn't present
      */
     fun getRequestByType(type: FormRequest.RequestType): FormRequest? =
-        topLevelObject.findProperty(type.requestLiteral).toFormElement()
+        topLevelObject?.findProperty(type.requestLiteral).toFormElement()
 
 }
