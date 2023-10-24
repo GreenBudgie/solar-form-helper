1d5861ad2547e90674d63f59d26ce620b942f8d2
Nikita Bundin <nbundin@solanteq.com>
2023-10-25 02:39:18 +0300

    SFH-1 Up version to intellij 2023.2.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ dependencies {
 
 // Configure Gradle IntelliJ Plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
 intellij {
-    version.set("2023.2")
+    version.set("2023.2.3")
     type.set("IC") // Target IDE Platform
 
     plugins.set(listOf(
