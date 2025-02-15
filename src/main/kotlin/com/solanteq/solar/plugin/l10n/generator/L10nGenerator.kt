cfe38cc1e3d31d1758342bea10d6a713696512fd
Nikita Bundin <nbundin@solanteq.com>
2025-02-15 23:46:33 +0300

    SFH-1 Implement and test l10n generation

--- src/main/kotlin/com/solanteq/solar/plugin/l10n/generator/L10nGenerator.kt
+++ src/main/kotlin/com/solanteq/solar/plugin/l10n/generator/L10nGenerator.kt
@@ -3,6 +3,9 @@ package com.solanteq.solar.plugin.l10n.generator
 import com.intellij.json.JsonElementTypes
 import com.intellij.json.psi.*
 import com.intellij.openapi.application.runUndoTransparentWriteAction
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiElement
+import com.intellij.psi.codeStyle.CodeStyleManager
 
 object L10nGenerator {
 
@@ -10,38 +13,117 @@ object L10nGenerator {
         key: String,
         value: String,
         file: JsonFile,
-        afterProperty: JsonProperty? = null,
+        placement: Placement,
     ) {
         val project = file.project
         val generator = JsonElementGenerator(project)
-        val generatedL10nProperty = generator.createProperty(key, value)
-
-        val topLevelObject = file.topLevelValue as? JsonObject
+        val generatedL10nProperty = generator.createProperty(key, "\"$value\"")
 
         runUndoTransparentWriteAction {
-            val effectiveTopLevelObject = if (topLevelObject == null) {
-                val generatedObject = generator.createObject("{\n}")
-                file.add(generatedObject) as JsonObject
-            } else {
-                topLevelObject
-            }
-            val insertAfter = if (afterProperty != null) {
-                val comma = afterProperty.nextSibling
-                if (!JsonPsiUtil.hasElementType(comma, JsonElementTypes.COMMA)) {
-                    effectiveTopLevelObject.addAfter(afterProperty, generator.createComma())
-                } else {
-                    comma
-                }
-            } else {
-                val lastProperty = effectiveTopLevelObject.propertyList.lastOrNull()
-                if (lastProperty != null) {
-                    effectiveTopLevelObject.addAfter(generator.createComma(), lastProperty)
-                } else {
-                    effectiveTopLevelObject.lastChild.prevSibling
-                }
-            }
-            effectiveTopLevelObject.addAfter(generatedL10nProperty, insertAfter)
+            writeL10n(file, generatedL10nProperty, placement, project, generator)
         }
+    }
+
+    private fun writeL10n(
+        file: JsonFile,
+        newProperty: JsonProperty,
+        placement: Placement,
+        project: Project,
+        generator: JsonElementGenerator,
+    ) {
+        val topLevelObject = getOrCreateTopLevelObject(file, generator)
+
+        val addedProperty = if (placement.property == null) {
+            JsonPsiUtil.addProperty(
+                topLevelObject,
+                newProperty,
+                placement.position == PlacementPosition.BEFORE
+            )
+        } else {
+            addPropertyWithAnchor(
+                placement,
+                topLevelObject,
+                newProperty,
+                placement.property,
+                generator
+            )
+        }
+
+        val range = addedProperty.textRange
+        CodeStyleManager.getInstance(project).reformatText(file, range.startOffset, range.endOffset)
+    }
+
+    private fun getOrCreateTopLevelObject(
+        file: JsonFile,
+        generator: JsonElementGenerator,
+    ): JsonObject {
+        val topLevelObject = file.topLevelValue as? JsonObject
+        if (topLevelObject != null) {
+            return topLevelObject
+        }
+
+        val generatedObject = generator.createObject("")
+        return file.add(generatedObject) as JsonObject
+    }
+
+    private fun addPropertyWithAnchor(
+        placement: Placement,
+        topLevelObject: JsonObject,
+        newProperty: JsonProperty,
+        anchorProperty: JsonProperty,
+        generator: JsonElementGenerator,
+    ): PsiElement {
+        val addedProperty = writeProperty(newProperty, anchorProperty, topLevelObject, placement, generator)
+
+        val rCurly = addedProperty.nextSibling
+        if (!JsonPsiUtil.hasElementType(rCurly, JsonElementTypes.R_CURLY)) {
+            topLevelObject.addAfter(generator.createComma(), addedProperty)
+        }
+
+        return addedProperty
+    }
+
+    private fun writeProperty(
+        newProperty: JsonProperty,
+        anchorProperty: JsonProperty,
+        topLevelObject: JsonObject,
+        placement: Placement,
+        generator: JsonElementGenerator,
+    ): PsiElement {
+        if (placement.position == PlacementPosition.BEFORE) {
+            return topLevelObject.addBefore(newProperty, anchorProperty)
+        }
+
+        val comma = anchorProperty.nextSibling
+        val effectiveComma = if (!JsonPsiUtil.hasElementType(comma, JsonElementTypes.COMMA)) {
+            topLevelObject.addAfter(generator.createComma(), anchorProperty)
+        } else comma
+
+        return topLevelObject.addAfter(newProperty, effectiveComma)
+    }
+
+    class Placement private constructor(
+        val property: JsonProperty?,
+        val position: PlacementPosition,
+    ) {
+
+        companion object {
+
+            fun before(property: JsonProperty) = Placement(property, PlacementPosition.BEFORE)
+
+            fun after(property: JsonProperty) = Placement(property, PlacementPosition.AFTER)
+
+            fun endOfFile() = Placement(null, PlacementPosition.AFTER)
+
+            fun startOfFile() = Placement(null, PlacementPosition.BEFORE)
+
+        }
+
+    }
+
+    enum class PlacementPosition {
+        BEFORE,
+        AFTER
     }
 
 }
