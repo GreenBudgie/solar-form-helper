c54b4357aa924395329bc96e18f8201560607f23
Nikita Bundin <nbundin@solanteq.com>
2025-02-23 10:31:46 +0300

    Add i18n bundle and refactor EditFormL10nDialog

--- src/main/kotlin/com/solanteq/solar/plugin/l10n/action/EditFormL10nDialog.kt
+++ src/main/kotlin/com/solanteq/solar/plugin/l10n/action/EditFormL10nDialog.kt
@@ -6,17 +6,16 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogWrapper
 import com.intellij.openapi.util.io.FileUtil
 import com.intellij.openapi.vfs.LocalFileSystem
+import com.intellij.psi.PsiFile
 import com.intellij.ui.GuiUtils
 import com.intellij.ui.JBColor
 import com.intellij.ui.TextFieldWithHistory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.fields.ExpandableTextField
-import com.intellij.ui.dsl.builder.COLUMNS_LARGE
-import com.intellij.ui.dsl.builder.columns
-import com.intellij.ui.dsl.builder.panel
-import com.intellij.ui.dsl.builder.text
+import com.intellij.ui.dsl.builder.*
 import com.intellij.ui.util.minimumHeight
 import com.solanteq.solar.plugin.asset.Icons
+import com.solanteq.solar.plugin.bundle.SolarBundle
 import com.solanteq.solar.plugin.element.FormRootFile
 import com.solanteq.solar.plugin.element.base.FormLocalizableElement
 import com.solanteq.solar.plugin.file.L10nFileType
@@ -39,8 +38,18 @@ class EditFormL10nDialog(
     val l10nData = mutableMapOf<FormL10nEntry, L10nData>()
 
     init {
-        title = "Edit Localization"
+        title = SolarBundle.message("dialog.l10n.edit.title")
         init()
+    }
+
+    fun getFile(fileChooserField: TextFieldWithHistory): JsonFile? {
+        val path = fileChooserField.selectedItem as String? ?: return null
+        return getFile(path)
+    }
+
+    fun getFile(path: String?): JsonFile? {
+        path ?: return null
+        return LocalFileSystem.getInstance().findFileByPath(path)?.toPsiFile(project) as? JsonFile
     }
 
     override fun createCenterPanel(): JComponent {
@@ -55,146 +64,186 @@ class EditFormL10nDialog(
             }
         return panel {
             entriesWithAllLocales.forEach { (rootForm, key, locales) ->
-                val formFullName = rootForm.fullName ?: "Unknown form"
+                val formFullName = rootForm.fullName ?: SolarBundle.message("dialog.l10n.edit.unknown.form")
                 group(JBLabel(formFullName, Icons.ROOT_FORM_ICON, SwingConstants.LEFT)) {
-                    row("Key: ") {
-                        label(key).bold()
-                    }
-                    locales.forEach { locale ->
-                        val l10nEntry = FormL10nEntry(rootForm, key, locale)
-                        val l10n = FormL10nSearch.search(project, l10nEntry)
-                            .inScope(project.projectScope())
-                            .findFirstObject()
-                        var initialL10nFile = if (l10n?.file == null) {
-                            val bestPlacement = FormL10nEditor.findBestPlacement(element, l10nEntry)
-                             bestPlacement?.file
-                        } else {
-                            l10n.file
-                        }
-                        row(locale.displayName) {
-                            val fileChooserField = TextFieldWithHistory()
-                            fileChooserField.setHistorySize(-1)
-                            fileChooserField.setEditable(false)
-                            if (l10n != null) {
-                                fileChooserField.isEnabled = false
-                            }
-                            if (initialL10nFile != null) {
-                                val selectedPath = FileUtil.toSystemDependentName(initialL10nFile.virtualFile.path)
-                                fileChooserField.text = selectedPath
-                                fileChooserField.selectedItem = selectedPath
-                                fileChooserField.addCurrentTextToHistory()
-                            }
-                            fileChooserField.renderer = object : DefaultListCellRenderer() {
-
-                                override fun getListCellRendererComponent(
-                                    list: JList<*>?,
-                                    value: Any?,
-                                    index: Int,
-                                    isSelected: Boolean,
-                                    cellHasFocus: Boolean,
-                                ): Component? {
-                                    super.getListCellRendererComponent(
-                                        list,
-                                        value,
-                                        index,
-                                        isSelected,
-                                        cellHasFocus
-                                    )
-
-                                    val file = getFile(value as String?)
-                                    if (file == null) {
-                                        text = "No file selected"
-                                        return this
-                                    }
-
-                                    text = file.name
-                                    icon = file.getIcon(0)
-
-                                    return this
-                                }
-
-                            }
-
-                            val fileBrowser = GuiUtils.constructFieldWithBrowseButton(fileChooserField) {
-                                val fileChooser = TreeFileChooserFactory.getInstance(project).createFileChooser(
-                                    "Choose Localization File",
-                                    getFile(fileChooserField) ?: initialL10nFile,
-                                    L10nFileType
-                                ) { file -> L10nLocale.getByFile(file) == locale }
-                                fileChooser.showDialog()
-                                val selectedFile = fileChooser.selectedFile
-                                if (selectedFile != null) {
-                                    val fileChooserField = fileChooserField
-                                    val selectedPath = FileUtil.toSystemDependentName(selectedFile.virtualFile.path)
-                                    fileChooserField.text = selectedPath
-                                    fileChooserField.selectedItem = selectedPath
-                                    fileChooserField.addCurrentTextToHistory()
-                                }
-                            }
-                            fileBrowser.minimumSize = Dimension(250, fileBrowser.minimumHeight)
-                            if (l10n != null) {
-                                fileBrowser.components.last().isEnabled = false
-                            }
-
-                            cell(fileBrowser)
-                                .widthGroup("fileBrowser")
-
-                            val valueField = expandableTextField({ mutableListOf(it) })
-                                .onChanged {
-                                    val data = l10nData[l10nEntry] ?: return@onChanged
-                                    var updatedStatus = data.currentStatus
-                                    if (it.text.isBlank()) {
-                                        updatedStatus = if (data.originalL10n != null) {
-                                            L10nEntryStatus.DELETED
-                                        } else {
-                                            L10nEntryStatus.ABSENT
-                                        }
-                                    } else {
-                                        updatedStatus = if (data.originalL10n != null) {
-                                            if (it.text == data.originalL10n.value) {
-                                                L10nEntryStatus.PRESENT
-                                            } else {
-                                                L10nEntryStatus.MODIFIED
-                                            }
-                                        } else {
-                                            L10nEntryStatus.NEW
-                                        }
-                                    }
-                                    if (updatedStatus != data.currentStatus) {
-                                        setStatus(data, updatedStatus)
-                                    }
-                                }
-                                .columns(COLUMNS_LARGE * 2)
-                            val initialValue = l10n?.value
-                            if (initialValue != null) {
-                                valueField.text(initialValue)
-                            }
-
-                            val status = if (l10n != null) {
-                                L10nEntryStatus.PRESENT
-                            } else {
-                                L10nEntryStatus.ABSENT
-                            }
-
-                            val statusLabel = label(status.text)
-                            statusLabel.component.foreground = status.color
-
-                            l10nData += l10nEntry to L10nData(
-                                fileChooserField = fileChooserField,
-                                valueField = valueField.component,
-                                statusLabel = statusLabel.component,
-                                originalL10n = l10n,
-                                originalStatus = status,
-                                currentStatus = status
-                            )
-                        }
-                    }
+                    l10nEntryGroup(rootForm, key, locales)
                 }
             }
         }
     }
 
-    private fun setStatus(data: L10nData, newStatus: L10nEntryStatus) {
+    private fun Panel.l10nEntryGroup(
+        rootForm: FormRootFile,
+        key: String,
+        locales: List<L10nLocale>,
+    ) {
+        row(SolarBundle.message("dialog.l10n.edit.key")) {
+            label(key).bold()
+        }
+        locales.forEach { locale ->
+            rowForLocale(rootForm, key, locale)
+        }
+    }
+
+    private fun Panel.rowForLocale(
+        rootForm: FormRootFile,
+        key: String,
+        locale: L10nLocale,
+    ) {
+        val l10nEntry = FormL10nEntry(rootForm, key, locale)
+        val originalL10n = FormL10nSearch.search(project, l10nEntry)
+            .inScope(project.projectScope())
+            .findFirstObject()
+        val initialL10nFile = if (originalL10n?.file == null) {
+            val bestPlacement = FormL10nEditor.findBestPlacement(element, l10nEntry)
+            bestPlacement?.file
+        } else {
+            originalL10n.file
+        }
+
+        row(locale.displayName) {
+            val fileChooserField = constructFileChooserField(originalL10n, initialL10nFile)
+            val fileBrowser = constructFileBrowser(fileChooserField, initialL10nFile, locale, originalL10n)
+            cell(fileBrowser)
+
+            val valueField = constructL10nValueField(l10nEntry, originalL10n)
+
+            val originalStatus = if (originalL10n != null) {
+                L10nEntryStatus.PRESENT
+            } else {
+                L10nEntryStatus.ABSENT
+            }
+
+            val statusLabel = label(originalStatus.text)
+            statusLabel.component.foreground = originalStatus.color
+            statusLabel.component.minimumSize = Dimension(80, statusLabel.component.minimumHeight)
+
+            l10nData += l10nEntry to L10nData(
+                fileChooserField = fileChooserField,
+                valueField = valueField.component,
+                statusLabel = statusLabel.component,
+                originalL10n = originalL10n,
+                originalStatus = originalStatus,
+                currentStatus = originalStatus
+            )
+        }
+    }
+
+    private fun Row.constructL10nValueField(
+        l10nEntry: FormL10nEntry,
+        originalL10n: FormL10n?,
+    ): Cell<ExpandableTextField> {
+        val valueField = expandableTextField({ mutableListOf(it) })
+            .onChanged { handleL10nValueTextChange(l10nEntry, it.text) }
+            .columns(COLUMNS_LARGE * 2)
+
+        val initialValue = originalL10n?.value
+        if (initialValue != null) {
+            valueField.text(initialValue)
+        }
+
+        return valueField
+    }
+
+    private fun handleL10nValueTextChange(
+        l10nEntry: FormL10nEntry,
+        text: String,
+    ) {
+        val data = l10nData[l10nEntry] ?: return
+
+        if (text.isBlank()) {
+            if (data.originalL10n != null) {
+                updateStatus(data, L10nEntryStatus.DELETED)
+                return
+            }
+
+            updateStatus(data, L10nEntryStatus.ABSENT)
+            return
+        }
+
+        if (data.originalL10n == null) {
+            updateStatus(data, L10nEntryStatus.NEW)
+            return
+        }
+
+        if (text == data.originalL10n.value) {
+            updateStatus(data, L10nEntryStatus.PRESENT)
+            return
+        }
+
+        updateStatus(data, L10nEntryStatus.MODIFIED)
+    }
+
+    private fun constructFileChooserField(
+        originalL10n: FormL10n?,
+        initialL10nFile: JsonFile?,
+    ): TextFieldWithHistory {
+        val fileChooserField = TextFieldWithHistory()
+        fileChooserField.setHistorySize(-1)
+        fileChooserField.setEditable(false)
+
+        if (originalL10n != null) {
+            fileChooserField.isEnabled = false
+        }
+
+        if (initialL10nFile != null) {
+            selectPath(initialL10nFile, fileChooserField)
+        }
+
+        fileChooserField.renderer = L10nFileListCellRenderer()
+        return fileChooserField
+    }
+
+    private fun constructFileBrowser(
+        fileChooserField: TextFieldWithHistory,
+        initialL10nFile: JsonFile?,
+        locale: L10nLocale,
+        originalL10n: FormL10n?,
+    ): JPanel {
+        val fileBrowser = GuiUtils.constructFieldWithBrowseButton(fileChooserField) {
+            onBrowseButtonClick(fileChooserField, initialL10nFile, locale)
+        }
+
+        fileBrowser.minimumSize = Dimension(250, fileBrowser.minimumHeight)
+        if (originalL10n != null) {
+            fileBrowser.components.last().isEnabled = false
+        }
+
+        return fileBrowser
+    }
+
+    private fun onBrowseButtonClick(
+        fileChooserField: TextFieldWithHistory,
+        initialL10nFile: JsonFile?,
+        locale: L10nLocale,
+    ) {
+        val fileChooser = TreeFileChooserFactory.getInstance(project).createFileChooser(
+            SolarBundle.message("dialog.l10n.edit.choose.file"),
+            getFile(fileChooserField) ?: initialL10nFile,
+            L10nFileType
+        ) { L10nLocale.getByFile(it) == locale }
+        fileChooser.showDialog()
+
+        val selectedFile = fileChooser.selectedFile
+        if (selectedFile == null) {
+            return
+        }
+
+        selectPath(selectedFile, fileChooserField)
+    }
+
+    private fun selectPath(selectedFile: PsiFile, fileChooserField: TextFieldWithHistory) {
+        val selectedPath = FileUtil.toSystemDependentName(selectedFile.virtualFile.path)
+        fileChooserField.text = selectedPath
+        fileChooserField.selectedItem = selectedPath
+        fileChooserField.addCurrentTextToHistory()
+    }
+
+    private fun updateStatus(data: L10nData, newStatus: L10nEntryStatus) {
+        if (data.currentStatus == newStatus) {
+            return
+        }
+
         data.statusLabel.text = newStatus.text
         data.statusLabel.foreground = newStatus.color
         data.currentStatus = newStatus
@@ -204,14 +253,35 @@ class EditFormL10nDialog(
         isOKActionEnabled = isAnyL10nModified && isAllFilesSelected
     }
 
-    fun getFile(fileChooserField: TextFieldWithHistory): JsonFile? {
-        val path = fileChooserField.selectedItem as String? ?: return null
-        return getFile(path)
-    }
+    private inner class L10nFileListCellRenderer : DefaultListCellRenderer() {
 
-    fun getFile(path: String?): JsonFile? {
-        path ?: return null
-        return LocalFileSystem.getInstance().findFileByPath(path)?.toPsiFile(project) as? JsonFile
+        override fun getListCellRendererComponent(
+            list: JList<*>?,
+            value: Any?,
+            index: Int,
+            isSelected: Boolean,
+            cellHasFocus: Boolean,
+        ): Component? {
+            super.getListCellRendererComponent(
+                list,
+                value,
+                index,
+                isSelected,
+                cellHasFocus
+            )
+
+            val file = getFile(value as String?)
+            if (file == null) {
+                text = SolarBundle.message("dialog.l10n.edit.file.not.selected")
+                return this
+            }
+
+            text = file.name
+            icon = file.getIcon(0)
+
+            return this
+        }
+
     }
 
     data class L10nData(
@@ -225,18 +295,18 @@ class EditFormL10nDialog(
 
     enum class L10nEntryStatus(val text: String, val color: JBColor, val modified: Boolean) {
 
-        PRESENT("present", JBColor.BLACK, false),
-        ABSENT("absent", JBColor.DARK_GRAY, false),
-        NEW("new", JBColor.GREEN, true),
-        DELETED("deleted", JBColor.RED, true),
-        MODIFIED("modified", JBColor.BLUE, true),
+        PRESENT(SolarBundle.message("l10n.entry.status.present"), JBColor.BLACK, false),
+        ABSENT(SolarBundle.message("l10n.entry.status.absent"), JBColor.DARK_GRAY, false),
+        NEW(SolarBundle.message("l10n.entry.status.new"), JBColor.GREEN, true),
+        DELETED(SolarBundle.message("l10n.entry.status.deleted"), JBColor.RED, true),
+        MODIFIED(SolarBundle.message("l10n.entry.status.modified"), JBColor.BLUE, true),
 
     }
 
     private data class L10nEntryAllLocales(
         val rootForm: FormRootFile,
         val key: String,
-        val locales: List<L10nLocale>
+        val locales: List<L10nLocale>,
     )
 
 }
